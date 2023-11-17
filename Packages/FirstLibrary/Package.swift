0bbc66f7296e4a251d7623a472572734b3b8c2b6
Jenny Tran <dutran@axon.com>
2023-11-17 15:14:46 +0700

    Update first lib zip

--- Packages/FirstLibrary/Package.swift
+++ Packages/FirstLibrary/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FirstLibrary",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,12 +15,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "FirstLibrary"),
         .binaryTarget(
-            name: "BinaryFirstLibrary",
-            url: "https://github.com/ttmdung203/MonoRepoWithSPM/releases/download/1.0.3/BinaryFirstLibrary.zip",
-            checksum: "dedd6512bcaef72c9e8225fad214e046fead7a4383ca3cc394837eccc4a17a6c"),
+            name: "FirstLibrary",
+            url: "https://github.com/ttmdung203/MonoRepoWithSPM/releases/download/1.0.4/FirstLibrary.zip",
+            checksum: "869c55c65e7b39417da6be31f0f1af5e9cd2c472ac5b7ed74518dac885053b23"),
         .testTarget(
             name: "FirstLibraryTests",
             dependencies: ["FirstLibrary"]),
